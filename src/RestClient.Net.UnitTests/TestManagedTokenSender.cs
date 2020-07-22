a54f543b0ea8ab098638d597cb70f9a9e0056d10
Christian Findlay <a@a.com>
2020-07-22 14:38:46 +1000

    Cleanup and renaming

--- src/RestClient.Net.UnitTests/TestManagedTokenSender.cs
+++ src/RestClient.Net.UnitTests/TestManagedTokenSender.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 
 #if !NET45
@@ -12,7 +13,7 @@ using System.Threading.Tasks;
 namespace RestClient.Net.UnitTests
 {
     [TestClass]
-    public class TestFunc
+    public class TestManagedTokenSender
     {
         #region Fields
         MockHttpMessageHandler mockHttpMessageHandler;
@@ -24,7 +25,7 @@ namespace RestClient.Net.UnitTests
         #endregion
 
         [TestMethod]
-        public async Task Test()
+        public async Task TestConcurrency()
         {
             string currentToken = null;
 
@@ -44,6 +45,9 @@ namespace RestClient.Net.UnitTests
                 //Track the tokens that are physically sent
                 SentBearerTokens.Add(t);
 
+                //Simulate some time being taken
+                Thread.Sleep(10);
+
                 //Increment fake time by 1 minute on each call
                 FakeNow += new TimeSpan(0, 1, 0);
             },
@@ -62,18 +66,22 @@ namespace RestClient.Net.UnitTests
             var tasks = new List<Task>();
 
             //Createa tasks
-            for (var i = 0; i < 61; i++)
+            for (var i = 0; i < 60; i++)
             {
                 tasks.Add(client.GetAsync<string>(new Uri(uri)));
             }
 
-            //Execute all in parallel
+            //Execute all 60 in parallel
             await Task.WhenAll(tasks);
 
             //Get all sent tokens that are equal to the first sent token
             var firstTokenList = SentBearerTokens.Where(t => t == $"Bearer {firstToken}").ToList();
 
+            //60 should have been sent within the hour
             Assert.AreEqual(60, firstTokenList.Count);
+
+            //1 should be sent afterwards representing the token refresh
+            Assert.AreEqual(1, SentBearerTokens.Count - firstTokenList.Count);
         }
 
         [TestInitialize]
