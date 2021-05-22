3ae7dfb333392afc285067a4bede55bbe0639758
Christian Findlay <email@email.email>
2021-05-22 11:49:04 +1000

    New uno samples

---
+++ src/RestClient.Net.UnoSample/RestClient.Net.UnoSample.macOS/Main.cs
@@ -0,0 +1,15 @@
+﻿using AppKit;
+
+namespace RestClient.Net.UnoSample.macOS
+{
+    internal static class MainClass
+	{
+        private static void Main(string[] args)
+		{
+			NSApplication.Init();
+			NSApplication.SharedApplication.Delegate = new App();
+			NSApplication.Main(args);  
+		}
+	}
+}
+
