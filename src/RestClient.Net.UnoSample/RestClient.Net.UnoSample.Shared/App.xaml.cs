a0d664f2478dfe60ece68410c1a09ea1017a4192
Christian Findlay <email@email.email>
2021-05-22 11:49:43 +1000

    Fix a thing

--- src/RestClient.Net.UnoSample/RestClient.Net.UnoSample.Shared/App.xaml.cs
+++ src/RestClient.Net.UnoSample/RestClient.Net.UnoSample.Shared/App.xaml.cs
@@ -47,8 +47,8 @@ namespace RestClient.Net.UnoSample
         /// Invoked when the application is launched normally by the end user.  Other entry points
         /// will be used such as when the application is launched to open a specific file.
         /// </summary>
-        /// <param name="e">Details about the launch request and process.</param>
-        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        /// <param name="args">Details about the launch request and process.</param>
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
         {
 #if DEBUG
             if (System.Diagnostics.Debugger.IsAttached)
@@ -75,7 +75,7 @@ namespace RestClient.Net.UnoSample
 
                 rootFrame.NavigationFailed += OnNavigationFailed;
 
-                if (e?.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                if (args?.PreviousExecutionState == ApplicationExecutionState.Terminated)
                 {
                     //TODO: Load state from previously suspended application
                 }
@@ -85,7 +85,7 @@ namespace RestClient.Net.UnoSample
             }
 
 #if !(NET5_0 && WINDOWS)
-            if (e?.PrelaunchActivated == false)
+            if (args?.PrelaunchActivated == false)
 #endif
             {
                 if (rootFrame.Content == null)
@@ -93,7 +93,7 @@ namespace RestClient.Net.UnoSample
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                    rootFrame.Navigate(typeof(MainPage), args.Arguments);
                 }
                 // Ensure the current window is active
                 _window.Activate();
